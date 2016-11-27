371ba556d7aa20a1101d86f21cbff94788bb78ac
adinyn.lu <adinyn@163.com>
2016-11-27 20:00:26 +0800

    migrations

--- hello.py
+++ hello.py
@@ -8,6 +8,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
 import os
+from flask_migrate import Migrate,MigrateCommand
 
 app = Flask(__name__)
 manager = Manager(app)
@@ -19,6 +20,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager.add_command('db',MigrateCommand)
+
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
